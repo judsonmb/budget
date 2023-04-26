746bb7f711a690d7bd57f176cf2a17311f5e4246
Judson Bandeira <judsonmelobandeira@gmail.com>
2023-04-26 10:14:15 -0300

    Creating the hasmany relations to all kinds of projects in customer model.

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
@@ -20,4 +21,28 @@ class Customer extends Model
         'phone',
         'address'
     ];
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function webProjects(): HasMany
+    {
+        return $this->hasMany(WebProject::class);
+    }
+
+     /**
+     * Get the comments for the blog post.
+     */
+    public function mobileProjects(): HasMany
+    {
+        return $this->hasMany(MobileProject::class);
+    }
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function desktopProjects(): HasMany
+    {
+        return $this->hasMany(DesktopProject::class);
+    }
 }
