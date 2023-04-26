fb941d832eab7988e67d786b990082afd651a6e8
Judson Bandeira <judsonmelobandeira@gmail.com>
2023-04-26 09:56:07 -0300

    Creating the web projects migration and model.

---
+++ app/Models/WebProject.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class WebProject extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'pages_number',
+        'has_login',
+        'has_payment',
+    ];
+
+    /**
+     * Get the user that owns the phone.
+     */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
